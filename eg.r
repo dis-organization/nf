0dea80ecfa364e51d6e86192690f402f9546c356
Michael Sumner <mdsumner@gmail.com>
2016-05-11 21:13:07 +1000

    clean up gris basic process

--- eg.r
+++ eg.r
@@ -1,30 +1,70 @@
 library(maptools)
 data(wrld_simpl)
 
+
+
+
+# COMBO 1. 
+# Create coordinate-first table and link to object table
+# 
+# COMBO 2. 
+# Badge unique vertices with new identifier .vx0
+# Split sptable into distinct Branch table and Coordinates table 
+# - optionally Branch has attributes on branch, as well as object, island status
+# - Coordinates table does not require object_ or island anymore island_, still needs order
+# 
+# COMBO 3. 
+# Split Coordinate table into branch-link-vertices and **unique** vertices
+# 
+
+
+
+
+## we expect that these attributes, taken together are "unique vertices" potentially shared by neighbours
+v_atts <- c("x_", "y_")
+
+pryr::object_size(wrld_simpl)
 library(spbabel) ## devtools::install_github("mdsumner/spbabel")
-## two tables
+## two tables (dat1, map1 - same as ggplot2 fortify)
 map <- subset(wrld_simpl, NAME %in% c("Botswana", "Lesotho", "Namibia","South Africa", "Swaziland"))
-dattable <- as.data.frame(map) %>% mutate(object_ = row_number())
-maptable <- sptable(map)
+#map <- wrld_simpl
+dat1<- as.data.frame(map) %>% mutate(object_ = row_number())
+## please note, we add unique vertex label here - but this is tidy the overall process, it's not used until COMBO 3
+map1 <- sptable(map) 
+map1 <- map1 %>% 
+  mutate(vertex_  = as.integer(factor(do.call(paste, select_(map1, .dots = v_atts)))))
+pryr::object_size(dat1, map1)
 
-pryr::object_size(dattable, maptable)
+## three tables (dat1, map2, map3)
+map2 <- map1 %>% distinct(object_, branch_, island_) 
+map3 <- map1 %>% dplyr::select(-object_, -island_)
+pryr::object_size(dat1, map2, map3)
 
-## three tables
-dattable1 <- dattable
-brtable1 <- maptable %>% distinct(object_, branch_, island_) 
-maptable1 <- maptable %>% dplyr::select(-object_, -island_)
-pryr::object_size(dattable1, brtable1, maptable1)
+## four tables (dat1, map2, map4, map5)
+map4 <- map3 %>% dplyr::select(branch_, vertex_)
+map5 <- map5 %>% distinct(x_, y_, vertex_)
 
-## four tables
-dattable2 <- dattable1
-brtable2 <- brtable1
-vertex <- maptable1 %>% mutate(vertex_ = row_number()) %>% dplyr::select(-branch_, -order_)
-br_vertex <- maptable1 %>% dplyr::select(branch_, order_) %>% mutate(vertex_ = row_number())
+pryr::object_size(dat1, map2, map4, map5)
 
-br_vertex$vertex_ <- vertex$vertex_ <- as.integer(factor(do.call(paste, dplyr::select(vertex, -vertex_))))
-pryr::object_size(dattable2, brtable2, vertex, br_vertex)
+## we can drive ggplot2 or sp with any of these combos
+library(ggplot2)
+combo1 <- dat1 %>% select(NAME, object_) %>% inner_join(map1)
+ggplot(combo1) + 
+  aes(x_, y_, group = branch_, fill = factor(NAME)) +   geom_polygon() 
+spplot(spFromTable(combo1))
 
-vertex <- distinct(vertex)
-pryr::object_size(dattable2, brtable2, vertex, br_vertex)
+combo2 <- dat1 %>% select(NAME, object_) %>% inner_join(map2) %>% inner_join(map3)
+ggplot(combo2) + 
+  aes(x_, y_, group = branch_, fill = factor(NAME)) +  geom_polygon()
+spplot(spFromTable(combo2))
 
-dattable2 %>% dplyr::select(NAME, object_) %>% inner_join(brtable2) %>% inner_join(br_vertex) %>% inner_join(vertex)
+combo3 <- dat1 %>% select(NAME, object_) %>% inner_join(map2) %>% inner_join(map4) %>% inner_join(map5)
+ggplot(combo3) + 
+  aes(x_, y_, group = branch_, fill = factor(NAME)) +  geom_polygon()
+spplot(spFromTable(combo2))
+
+
+
+
+
+
