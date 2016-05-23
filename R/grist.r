eb3b522b822508ee17d3079132138008ed04d171
Michael Sumner <mdsumner@gmail.com>
2016-05-23 21:34:48 +1000

    more test with new data

--- R/grist.r
+++ R/grist.r
@@ -1,9 +1,4 @@
 
-## we expect that these attributes, taken together are "unique vertices" potentially shared by neighbours
-v_atts <- c("x_", "y_")
-
-library(spbabel) ## devtools::install_github("mdsumner/spbabel")
-
 gris2 <- function(x) {
   ## two tables (dat1, map1 - same as ggplot2 fortify)
   #map <- subset(x, NAME %in% c("Botswana", "Lesotho", "Namibia","South Africa", "Swaziland"))
@@ -22,24 +17,9 @@ gris2 <- function(x) {
   
   list(o = dat1, b = map2, bXv = map4, v = map5)
 }
-
-
-
-
-library(maptools)
-data(wrld_simpl)
-library(dplyr)
-x <- gris2(wrld_simpl)
-verts <- with(x, o %>% filter(NAME == "Japan") %>% select(object_) %>% inner_join(b) %>% inner_join(bXv) %>% inner_join(v))
-library(ggplot2)
-ggplot(verts) + aes(x = x_, y = y_, fill = branch_, group = branch_) + geom_polygon()
-
 pairs0 <- function (x) {
   data_frame(s0 = head(x, -1), s1 = tail(x, -1))
 }
-# x$s <- bind_rows(
-#    with(x,lapply(split(bXv, bXv$branch_), function(a) pairs0(a$vertex_))), 
-#    .id = "branch_")
 
 
 grist <- function(x) {
@@ -50,19 +30,6 @@ grist <- function(x) {
   }
   bind_rows(br)
 } 
-
-
-## say we add the segments, then subset
-x$s <- grist(x)
-xa <- list(o =x$o %>% filter(NAME == "Japan"))
-xa$b <- inner_join(x$b, xa$o %>% select(object_))
-xa$bXv <- inner_join(x$bXv, xa$b %>% select(branch_))
-xa$v <- inner_join(x$v, xa$bXv %>% select(vertex_)) %>% distinct()
-xa$s <- inner_join(x$s, xa$b %>% select(branch_))
-
-verts <- with(xa, o %>% select(object_) %>% inner_join(b) %>% inner_join(bXv) %>% inner_join(v))
-library(ggplot2)
-ggplot(verts) + aes(x = x_, y = y_, fill = branch_, group = branch_) + geom_polygon()
 
 seg2struct <- function(x) {
   v <- x$v
@@ -75,30 +42,3 @@ seg2struct <- function(x) {
 }
 
 
-#p <- RTriangle::pslg(xa$v %>% dplyr::select(x_, y_) %>% as.matrix())
-#S = seg2struct(xa) %>% as.matrix()
-
-
-tr <- RTriangle::triangulate(p = RTriangle::pslg(xa$v %>% dplyr::select(x_, y_) %>% as.matrix(), 
-                                                 S = seg2struct(xa) %>% as.matrix()), 
-                                                 a = 0.1)
-plot(tr$P, pch = ".")
-apply(tr$T, 1, function(a) lines(tr$P[a, ]))
-
-
-
-
-library(RTriangle)
-tr <- RTriangle::triangulate(p = RTriangle::pslg(x$v %>% dplyr::select(x_, y_) %>% as.matrix()), 
-                             S = cbind(br$s0, br$s1))
-
-#S = split(br, br$branch_)[[1]] %>% dplyr::select(s0, s1) %>%  as.matrix())
-
-
-
-
-split(x$s, x$s$branch_)[[1]]
-library(RTriangle)
-
-RTriangle::triangulate(p = pslg(x$v %>% dplyr::select(x_, y_) %>% as.matrix()), 
-                       S = x$s %>% as.matrix())
