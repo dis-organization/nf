a267f309271d493d3f51314d966ea050b36012aa
Michael Sumner <mdsumner@gmail.com>
2016-05-25 21:17:17 +1000

    segments

--- R/grist.r
+++ R/grist.r
@@ -1,4 +1,49 @@
+library(spbabel) ## devtools::install_github("mdsumner/spbabel")
+library(dplyr)
+source("R/grist.r")
+library(maptools)
+data(wrld_simpl)
+#install.packages("rworldxtra")
+library(rworldxtra)
+data(countriesHigh)
 
+d <- countriesHigh[1, ] ; plot(d)
+d1<- as.data.frame(d) %>% mutate(object_ = row_number())
+map1 <- sptable(d) 
+
+x <- gris2(d1, map1)
+
+verts <- with(x, o %>% filter(NAME == "Japan") %>% select(object_) %>% inner_join(b) %>% inner_join(bXv) %>% inner_join(v))
+library(ggplot2)
+ggplot(verts) + aes(x = x_, y = y_, fill = branch_, group = branch_) + geom_polygon()
+
+pairs1 <- 
+function (x) {
+  as.vector(t(cbind(head(x, -1), s1 = tail(x, -1))))
+}
+
+## this is 6 tables v, sXv, s, bXs, b, o
+gris3 <- function(dat1, map1) {
+  ## we expect that these attributes, taken together are "unique vertices" potentially shared by neighbours
+  v_atts <- c("x_", "y_")
+
+  map1 <- map1 %>% 
+    mutate(vertex_  = as.integer(factor(do.call(paste, select_(map1, .dots = v_atts)))))
+  sXv <- bind_rows(lapply(split(map1, map1$branch_), function(a) data_frame(vertex_ = pairs1(a$vertex_), branch_ = a$branch_[1])))
+  sXv$segment_ <- rep(seq(nrow(sXv)/2), each = 2)
+  s <- sXv %>% distinct(segment_)
+  bXs <- sXv %>% distinct(segment_, .keep_all = TRUE) %>% select(-vertex_)
+  map2 <- map1 %>% distinct(object_, branch_, island_) %>% dplyr::select(object_, branch_, island_)
+
+  
+    map3 <- map1 %>% dplyr::select(-object_, -island_)
+  
+  ## four tables (dat1, map2, map4, map5)
+  map4 <- map3 %>% dplyr::select(branch_, vertex_, order_)
+  map5 <- map3 %>% distinct(x_, y_, vertex_) %>% dplyr::select(x_, y_, vertex_)
+  
+  list(o = dat1, b = map2, bXv = map4, v = map5)
+}
 
 #' Title
 #'
